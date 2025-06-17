5b21d68893b653476194d0c5ae51782b14d70516
H3Git <hellenmayara2504@gmail.com>
2025-06-17 00:19:32 +0000

    Super trunfo novato finalizado

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,2 +1,45 @@
 #include <stdio.h>
 
+int main(){
+    printf("Desafio super trunfo - Países!\n");
+
+    int populacao, turisticos;
+    float area;
+    float pib;
+    char estado[10];
+    char codigo[3];
+    char nome[10];
+
+    printf("Carta 1: Brasil \n");
+
+    printf("Digite o Nome do Estado: ");
+    scanf("%s", &estado);
+
+    printf("Digite o Código da carta: ");
+    scanf("%s", &codigo);
+
+    printf("Digite o Nome da cidade: ");
+    scanf("%s", &nome);
+
+    printf("Digite a População: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a Área: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+
+    printf("Digite o Número de pontos turísticos: ");
+    scanf("%d", &turisticos);
+
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome da cidade: %s\n", nome);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Números de pontos turísticos: %d\n", turisticos);
+
+    return 0;
+}
